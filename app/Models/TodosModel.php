042a49f512a2648a063b353af1692163f59b89cb
tegarpenemuan <tegarpenemuan@gmail.com>
2024-03-27 20:58:10 +0700

    Create-model-todos (#5)

---
+++ app/Models/TodosModel.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class TodosModel extends Model
+{
+    protected $table            = 'todos';
+    protected $primaryKey       = 'todos_id';
+    protected $useAutoIncrement = true;
+    protected $returnType       = 'object';
+    protected $useSoftDeletes   = false;
+    protected $protectFields    = true;
+    protected $allowedFields    = [
+        'todo_title','todo_description','todo_status'
+    ];
+
+    protected bool $allowEmptyInserts = false;
+
+    // Dates
+    protected $useTimestamps = false;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
+}
