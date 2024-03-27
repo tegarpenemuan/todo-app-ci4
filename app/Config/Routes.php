59614a4b315d680eba35f10e9a249de15ac84b98
tegarpenemuan <tegarpenemuan@gmail.com>
2024-03-27 20:49:22 +0700

    Create-db (#3)

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,21 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+// Membuat database
+$routes->get('create-db', function() {
+	$forge = \Config\Database::forge();
+	if ($forge->createDatabase('todo_app_ci4'))
+	{
+		echo 'Database created!';
+	}
+});
+
+// Menghapus database
+$routes->get('drop-db', function() {
+	$forge = \Config\Database::forge();
+	if ($forge->dropDatabase('todo_app_ci4'))
+	{
+		echo 'Database deleted!';
+	}
+});
